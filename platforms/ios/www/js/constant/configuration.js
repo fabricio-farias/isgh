800d74367c98efe833d3ce662f7af03bfa2a7df3
Fabricio <fabriciorelicario@gmail.com>
2015-09-08 09:29:48 -0300

    updating com o github server ultimas atualizações

---
+++ platforms/ios/www/js/constant/configuration.js
@@ -0,0 +1,7 @@
+angular.module('isgh.Configuration', []).constant("Configuration", {
+	
+	url_appserver: "http://177.136.74.42/appserver/", 
+	url_intranet: "http://177.136.74.42/intranet/",
+	url_site: "http://177.136.74.42/site/",
+	url_procseletivo: "http://177.136.74.42/processos_seletivos/"
+});
